846d961d39b72927bfcb9cc6d2d851474c41b664
break-jahun <jh.lee@break.co.kr>
2023-07-23 13:55:50 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const apiUrl = "https://samesitetestapi.vercel.app";
 
 function App() {
+  const [token, setToken] = useState("디폴트");
+
+  const handleClickNoConfig = () => {
+    fetch(`${apiUrl}/api/samesitedefault`, {
+      method: "post",
+      credentials: "include",
+    });
+  };
+
+  const handleClickNone = () => {
+    fetch(`${apiUrl}/api/samesitenone`, {
+      method: "post",
+      credentials: "include",
+    });
+  };
+  const handleClickLax = () => {
+    fetch(`${apiUrl}/api/samesitelax`, {
+      method: "post",
+      credentials: "include",
+    });
+  };
+  const handleLogout = () => {
+    fetch(`${apiUrl}/api/logout`, {
+      method: "post",
+      credentials: "include",
+    });
+  };
+  const handleRequestLogin = async () => {
+    const result = await fetch(`${apiUrl}/api/login`, {
+      method: "get",
+      credentials: "include",
+    });
+    const data = await result.json();
+    const newToken = data?.token;
+    setToken(newToken);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <button onClick={handleClickNoConfig}>
+          same site 설정 안하고 쿠키 발급
+        </button>
+        <button onClick={handleClickNone}>same site none 에서 쿠키 발급</button>
+        <button onClick={handleClickLax}>same site lax 에서 쿠키 발급</button>
+        <button onClick={handleLogout}>로그아웃</button>
+        <button onClick={handleRequestLogin}>로그인 GET 요청</button>
+      </div>
+      <div>
+        <h1>{token}</h1>
+      </div>
     </div>
   );
 }
